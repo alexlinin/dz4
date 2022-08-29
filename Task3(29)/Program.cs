4aa441bb7a8e27abc8f3f93f530fb1fe2884756c
Alexandr <lininan@mail.ru>
2022-08-29 23:05:10 +0300

    Выполнил 3 задание через генерацию массива

---
+++ Task3(29)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29:
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Укажите количество элементов массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] numberArray = new int[number];
+
+for (int i = 0; i < number; i++)
+{
+    numberArray[i] = new Random().Next(0,9);
+}
+
+Console.WriteLine($"Результат генерации массива: [ {String.Join(", ", numberArray) } ]");
